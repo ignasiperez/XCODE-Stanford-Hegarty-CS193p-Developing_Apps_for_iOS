47f1bb1e23b402157c2391582bee238d1e5e0fd7
Ignasi  Perez <perezignasi@gmail.com>
2020-05-22 22:59:33 +0200

    Lec01 Memorize - CardView created

--- Lec01-Memorize/Memorize/ContentView.swift
+++ Lec01-Memorize/Memorize/ContentView.swift
@@ -6,19 +6,44 @@
 //  Copyright © 2020 Ignasi Perez-Valls. All rights reserved.
 //
 
+
+
 import SwiftUI
 
 struct ContentView: View {
-    var body: Text {
-        Text("👻")
-    }
-}
+  var body: some View {
+    HStack {
+      ForEach(0..<4) { index in
+        CardView(isFaceUp: false)
+      }
+    } // HStack
+      .padding()
+      .foregroundColor(Color.orange)
+      .font(Font.largeTitle)
+  } // body
+} // ContentView
 
+
+struct CardView: View {
+  var isFaceUp: Bool
+  
+  var body: some View {
+    ZStack {
+      if isFaceUp {
+              RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+              RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+              Text("👻")
+      } else {
+        RoundedRectangle(cornerRadius: 10.0).fill()
+      }
+    } // ZStack
+  } // body
+} // CardView
 
 
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }

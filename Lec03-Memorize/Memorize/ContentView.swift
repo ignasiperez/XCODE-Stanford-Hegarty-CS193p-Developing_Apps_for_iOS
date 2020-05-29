9c9f2164ac639e6e19985d52700062b9debbd29e
Ignasi  Perez <perezignasi@gmail.com>
2020-05-29 23:34:30 +0200

    Lec03 Memorize created

---
+++ Lec03-Memorize/Memorize/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by ignasiperez.com on 19/05/2020.
+//  Copyright © 2020 Ignasi Perez-Valls. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+  var viewModel: EmojiMemoryGame
+  
+  var body: some View {
+    HStack {
+      ForEach(viewModel.cards) { card in
+        CardView(card: card).onTapGesture {
+          self.viewModel.choose(card: card)}
+      }
+    } // HStack
+      .padding()
+      .foregroundColor(Color.orange)
+      .font(Font.largeTitle)
+  } // body
+} // EmojiMemoryGameView
+
+
+struct CardView: View {
+  var card: MemoryGame<String>.Card
+  
+  var body: some View {
+    ZStack {
+      if card.isFaceUp {
+              RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+              RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+        Text(card.content)
+      } else {
+        RoundedRectangle(cornerRadius: 10.0).fill()
+      }
+    } // ZStack
+  } // body
+} // CardView
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+  }
+}
---
+++ Lec03-Memorize/Memorize/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  Memorize
+//
+//  Created by ignasiperez.com on 29/05/2020.
+//  Copyright © 2020 Ignasi Perez-Valls. All rights reserved.
+//
+
+import Foundation
+
+struct Test2 {
+  
+}

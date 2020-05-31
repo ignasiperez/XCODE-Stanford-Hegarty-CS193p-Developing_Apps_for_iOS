9f892ec8ee93ebecb0f3a0afa12fa3f685cf5edb
Ignasi  Perez <perezignasi@gmail.com>
2020-05-31 21:52:15 +0200

    Lec03 Memorize GeometryProxy + Coding style

--- Lec03-Memorize/Memorize/ContentView.swift
+++ Lec03-Memorize/Memorize/ContentView.swift
@@ -20,7 +20,6 @@ struct EmojiMemoryGameView: View {
     } // HStack
       .padding()
       .foregroundColor(Color.orange)
-      .font(Font.largeTitle)
   } // body
 } // EmojiMemoryGameView
 
@@ -29,17 +28,44 @@ struct CardView: View {
   var card: MemoryGame<String>.Card
   
   var body: some View {
+    GeometryReader { geometry in
+      self.body(for: geometry.size)
+    }
+  } // body
+  
+  
+  func body(for size: CGSize) -> some View {
     ZStack {
-      if card.isFaceUp {
-              RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-              RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-        Text(card.content)
+      if self.card.isFaceUp {
+        RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+        RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+        Text(self.card.content)
       } else {
-        RoundedRectangle(cornerRadius: 10.0).fill()
+        RoundedRectangle(cornerRadius: cornerRadius).fill()
       }
     } // ZStack
-  } // body
+      .font(Font.system(size: fontSize(for: size)))
+  }
+  
+  
+  
+  // ************************************************************
+  // MARK: - Drawing Constants
+  //
+
+  let cornerRadius: CGFloat = 10.0
+  let edgeLineWidth: CGFloat = 3
+  
+  func fontSize(for size: CGSize) -> CGFloat {
+    min(size.width, size.height) * 0.75
+  }
 } // CardView
+
+
+
+
+
+
 
 
 struct ContentView_Previews: PreviewProvider {

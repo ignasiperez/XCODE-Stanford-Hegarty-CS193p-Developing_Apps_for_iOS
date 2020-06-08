3342d49204dfa20054afb14d49543bd3df02b197
Ignasi  Perez <perezignasi@gmail.com>
2020-06-08 20:35:38 +0200

    Lec05 Memorize Pie

---
+++ Lec05-Memorize/Memorize/ContentView.swift
@@ -0,0 +1,84 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by ignasiperez.com on 19/05/2020.
+//  Copyright © 2020 Ignasi Perez-Valls. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+  @ObservedObject var viewModel: EmojiMemoryGame
+  
+  var body: some View {
+    Grid(viewModel.cards) { card in
+      CardView(card: card).onTapGesture {
+        self.viewModel.choose(card: card)
+      }
+      .padding(5)
+    } // Grid
+      .padding()
+      .foregroundColor(Color.orange)
+  } // body
+} // EmojiMemoryGameView
+
+
+struct CardView: View {
+  var card: MemoryGame<String>.Card
+  
+  var body: some View {
+    GeometryReader { geometry in
+      self.body(for: geometry.size)
+    }
+  } // body
+  
+  
+  private func body(for size: CGSize) -> some View {
+    ZStack {
+      if self.card.isFaceUp {
+        RoundedRectangle(cornerRadius: cornerRadius)
+          .fill(Color.white)
+        RoundedRectangle(cornerRadius: cornerRadius)
+          .stroke(lineWidth: edgeLineWidth)
+        Pie(startAngle: Angle.degrees(0-90),
+            endAngle: Angle.degrees(110-90),
+            clockwise: true)
+          .padding(5)
+          .opacity(0.4)
+        Text(self.card.content)
+      } else {
+        if !card.isMatched {
+          RoundedRectangle(cornerRadius: cornerRadius)
+          .fill()
+        }
+      }
+    } // ZStack
+      .font(Font.system(size: fontSize(for: size)))
+  }
+  
+  
+  
+  
+  
+  // ************************************************************
+  // MARK: - Drawing Constants
+  //
+  
+  private let cornerRadius: CGFloat = 10.0
+  private let edgeLineWidth: CGFloat = 3
+  
+  private func fontSize(for size: CGSize) -> CGFloat {
+    min(size.width, size.height) * 0.70
+  }
+} // CardView
+
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    let game = EmojiMemoryGame()
+    game.choose(card: game.cards[0])
+    return EmojiMemoryGameView(viewModel: game)
+  }
+}

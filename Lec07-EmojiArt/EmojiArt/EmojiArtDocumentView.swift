585097b338d919ce05b99e8e961dc4c0778e5101
Ignasi  Perez <perezignasi@gmail.com>
2020-07-04 21:21:15 +0200

    Lec07 EmojiArt - Model - func addEmoji(_:, x:, y:, size:)

--- Lec07-EmojiArt/EmojiArt/EmojiArtDocumentView.swift
+++ Lec07-EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -14,11 +14,23 @@ struct EmojiArtDocumentView: View {
   
   
   var body: some View {
-    HStack {
-      ForEach(EmojiArtDocument.palette.map { String($0) }, id:\.self) { emoji in
-        Text(emoji)
-      }
-    }
+    VStack {
+      ScrollView(.horizontal) {
+        HStack {
+          ForEach(EmojiArtDocument.palette.map { String($0) }, id:\.self) { emoji in
+            Text(emoji)
+              .font(Font.system(size: self.defaultEmojiSize))
+          }
+        }
+      } // ScrollView
+      .padding(.horizontal)
+      Rectangle()
+        .foregroundColor(.yellow)
+        .edgesIgnoringSafeArea([.horizontal, .bottom])
+    } // VStack
   }
+  
+  private let defaultEmojiSize: CGFloat = 48
+  
 }
 
